6a4520a7bd97802376f0757e3342c511f79d14be
Armando Cervantes <Familyguy350z@yahoo.com>
2015-04-21 19:40:16 -0700

    exercise

---
+++ exercise.java
@@ -0,0 +1,21 @@
+
+public class exercise {
+
+	public static void main(String[] args){
+		
+		String firstName;
+		String lastName;
+		int score;
+		
+		
+		student s1= new student();
+		student s2= new student();
+		
+		s1.firstName = Georia;
+		s1.lasName = Dome;
+		
+		s2.firstName = Abby;
+		s2.lastName = Stracksen;
+	}
+	
+}
